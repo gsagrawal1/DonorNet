e5fc9dfb1035de106fa92d2f67f26a025b3a79b3
Ghanshyam Agrawal <ghanshyamagrawal456@gmail.com>
2025-01-10 21:03:29 +0545

    Initialize Client side

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Rout } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route to="/" element={<Home />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
